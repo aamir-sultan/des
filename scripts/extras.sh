54f4535ce1e7c2c06b0afb9ffaad746e117d437b
aamir-sultan <asultan.here@gmail.com>
2023-11-12 01:03:33 +0500

    chore: add fd installation support

--- scripts/extras.sh
+++ scripts/extras.sh
@@ -30,6 +30,10 @@ else
   echo Setup file for extra already exists 
 fi
 
+# Fields=("name, url, dload_path, dload_tool, dload_switches, post_process_cmds")
+extra_bins=(
+  "fd, https://github.com/sharkdp/fd/releases/download/v8.7.1/fd-v8.7.1-i686-unknown-linux-musl.tar.gz, $TOOLBOX, wget, --directory-prefix=$TOOLBOX, tar xzf dfile"
+)
 
 # Fields=("git_url, path, hash_or_branch, clone_switches, install_switches/install_command")
 extras=(
@@ -50,29 +54,82 @@ for ((i=0; $i < ${#extras[*]}; i++)) do
   clone_switches=${repo_info[4]}
   install_command=${repo_info[5]}
 
-  echo "Name: $name"
-  echo "URL: $url"
-  echo "Path: $path"
-  echo "Hash: $hash"
-  echo "Clone switches: $clone_switches"
-  echo "Install command: $install_command"
+  echo "Name:              $name"
+  echo "URL:              $url"
+  echo "Path:             $path"
+  echo "Hash:             $hash"
+  echo "Clone switches:   $clone_switches"
+  echo "Install command:  $install_command"
   echo -------------------------------------
 
   # Cloning setup
   if [ ! -d $path ]; then
     git clone $clone_switches $url $path 
+    echo Entering $path
+    cd $path; git fetch; git checkout $hash
+    # Installing setup
+    if [ ! -z "$install_command" ]; then
+      eval $install_command
+    fi
   else
     echo -e "\n$name Already available at $path"
   fi
 
-  echo Entering $path
-  cd $path; git fetch; git checkout $hash
 
-  # Installing setup
-  if [ ! -z "$install_command" ]; then
-    eval $install_command
+  cd -
+  echo -------------------------------------------------------------------------------
+done
+
+for ((i=0; $i < ${#extra_bins[*]}; i++)) do
+  echo -------------------------------------------------------------------------------
+  temp=(${extra_bins[$i]})
+  echo "Array elements: ${temp[@]}"
+  IFS=, read -ra bin_info <<< "${extra_bins[$i]}"
+
+  name=${bin_info[0]}
+  url=${bin_info[1]}
+  dload_path=${bin_info[2]}
+  # file_extension are related to dload_path and used in the following commands
+  filename=$(basename "$url")
+
+  dload_tool=${bin_info[3]}
+  dload_switches=${bin_info[4]}
+  post_proc_cmd=${bin_info[5]}
+
+  setup_path=$dload_path/$name
+  file_path=$dload_path/$filename
+
+  echo "Name:                      $name"
+  echo "URL:                      $url"
+  echo "Filename:                  $filename"
+  echo "Download Path:            $dload_path"
+  echo "Download tool:            $dload_tool"
+  echo "Download switches:        $dload_switches"
+  echo "Post Processing command:  $post_proc_cmd"
+  echo "Setup Path:               $setup_path"
+  echo "File Path:                $file_path"
+  echo -------------------------------------------
+
+  # Cloning setup
+  if [ ! -d $setup_path ]; then
+    if [ ! -f $file_path ]; then
+      $dload_tool $url $dload_switches 
+    fi
+    cd $dload_path; mv $filename dfile
+    echo "Entered $(pwd)"
+
+    # Installing setup
+    if [ ! -z "$post_proc_cmd" ]; then
+      eval $post_proc_cmd
+      mv $name* $name
+    fi
+      rm -rf dfile $filename
+
+  else
+    echo -e "\n$name Already available at $dload_path"
   fi
 
+    cd -
   echo -------------------------------------------------------------------------------
 done
 

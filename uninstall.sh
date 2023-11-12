56d4235fa42510ab82573ca0b50b3939274821ba
aamir-sultan <asultan.here@gmail.com>
2023-11-13 00:58:11 +0500

    chore: fixed the installation of nvim

--- uninstall.sh
+++ uninstall.sh
@@ -1,12 +1,49 @@
 #!/bin/bash
 
+# Define a function for colored echo
+c_echo() {
+  local color="$1"
+  local message="$2"
+  local reset="\033[0m"  # Reset to default text color
+
+  case "$color" in
+    "red")
+      echo -e "\033[31m$message$reset"
+      ;;
+    "green")
+      echo -e "\033[32m$message$reset"
+      ;;
+    "yellow")
+      echo -e "\033[33m$message$reset"
+      ;;
+    "blue")
+      echo -e "\033[34m$message$reset"
+      ;;
+    "magenta")
+      echo -e "\033[35m$message$reset"
+      ;;
+    "cyan")
+      echo -e "\033[36m$message$reset"
+      ;;
+    *)
+      echo "Invalid color. Usage: colored_echo <color> <message>"
+      return 1
+      ;;
+  esac
+}
+
 source ./path_exports.sh
+
+if [ -f $TOOLBOX/fzf/uninstall ];
+then
+  source $TOOLBOX/fzf/uninstall
+fi
+
 if [ -f ./dotfiles/uninstall.sh ];
 then
   source ./dotfiles/uninstall.sh
 fi
 
-source $TOOLBOX/fzf/uninstall
 DIRS=""
 DIRS+="$HOME/.local/des "
 DIRS+="toolbox "
@@ -19,3 +56,7 @@ FILES=""
 FILES+="*.log "
 
 rm -rf $DIRS $FILES
+
+c_echo "green" "*******************************************************************************"
+c_echo "red"   "                              FINISHED Uninstallation" 
+c_echo "green" "*******************************************************************************"

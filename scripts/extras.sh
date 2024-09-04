1d3ed9dd45caa6e9387a33e1c72f895a7787914a
aamir-sultan <asultan.here@gmail.com>
2024-09-04 13:15:15 +0500

    chore: keep vivid when non ssh

--- scripts/extras.sh
+++ scripts/extras.sh
@@ -23,7 +23,7 @@ extra_bins=(
   "nvim, https://github.com/neovim/neovim/releases/download/v0.9.5/nvim-linux64.tar.gz, $TOOLBOX, wget, --directory-prefix=$TOOLBOX, tar xzf dfile"
   "fd, https://github.com/sharkdp/fd/releases/download/v8.7.1/fd-v8.7.1-i686-unknown-linux-musl.tar.gz, $TOOLBOX, wget, --directory-prefix=$TOOLBOX, tar xzf dfile"
   "bat, https://github.com/sharkdp/bat/releases/download/v0.24.0/bat-v0.24.0-i686-unknown-linux-musl.tar.gz, $TOOLBOX, wget, --directory-prefix=$TOOLBOX, tar xzf dfile"
-  # "vivid, https://github.com/sharkdp/vivid/releases/download/v0.9.0/vivid-v0.9.0-x86_64-unknown-linux-musl.tar.gz, $TOOLBOX, wget, --directory-prefix=$TOOLBOX, tar xzf dfile"
+  "vivid, https://github.com/sharkdp/vivid/releases/download/v0.9.0/vivid-v0.9.0-x86_64-unknown-linux-musl.tar.gz, $TOOLBOX, wget, --directory-prefix=$TOOLBOX, tar xzf dfile"
   # "uctags, https://github.com/universal-ctags/ctags-nightly-build/releases/download/2023.12.03%2B684ed1d057eef7dd5b81a2d3a7576f96d4a827b9/uctags-2023.12.03-linux-x86_64.tar.xz, $TOOLBOX, wget, --directory-prefix=$TOOLBOX, tar xf dfile"
 )
 
